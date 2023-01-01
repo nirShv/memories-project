4a018ca537732bb52b755444c1e16a43006ac8f0
nirShv <nir1shv@gmail.com>
2023-01-01 13:38:31 +0200

    backend setup and basic routes

---
+++ backend/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createAt: {
+        type: Date,
+        default: new Date()
+    },
+
+})
+
+const PostMassage = mongoose.model('PostMessage', postSchema)
+
+export default PostMassage
