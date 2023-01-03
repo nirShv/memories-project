f34277e3e96d723939c29f9b9cc07486cbb3404c
nirShv <nir1shv@gmail.com>
2023-01-03 18:46:13 +0200

    build

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,11 @@ app.get('/',(req,res)=>{
     res.send('Hello to Moments API')
 })
 
-// const CONNECTION_URL = 'mongodb+srv://nirProjects:nirProjects123@cluster0.bpmfyqy.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://nirProjects:nirProjects123@cluster0.bpmfyqy.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
