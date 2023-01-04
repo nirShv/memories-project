45ae977f6983dcbd06e51faa8533b7f9989f9999
nirShv <nir1shv@gmail.com>
2023-01-04 13:55:31 +0200

    test 23

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,9 @@ app.get('/**', (req, res) => {
 
 // mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 // mongoose.set('strictQuery', false)
+
+console.log('process.env.CONNECTION_URL',process.env.CONNECTION_URL);
+console.log('process.env.PORT',process.env.PORT);
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
     .catch((error) => console.log(error.message))
