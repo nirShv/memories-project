01c60f80b98f4ffcb7207f92df6a973a09669b24
nirShv <nir1shv@gmail.com>
2023-01-04 11:51:45 +0200

    test 15

--- backend/index.js
+++ backend/index.js
@@ -40,13 +40,13 @@ app.use('/posts', postRoutes)
 // })
 
 
-app.get('/', (req, res) => {
+app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 // mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
     .catch((error) => console.log(error.message))
 
 // mongoose.set('useFindAndModify', false)
